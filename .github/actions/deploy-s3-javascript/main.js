d0bc91f7f8cb3b840b9636e411797205dd031847
falamarcao <falamarcao@gmail.com>
2024-10-09 18:43:54 -0300

    get code to custom action work

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.
+    core.notice('Hello from my custom JavaScript Action!')
  }
 
 run();
