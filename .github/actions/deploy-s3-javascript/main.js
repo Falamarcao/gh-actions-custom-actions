9e2e14f9984ea030b6806d69b0ecd0537f052218
falamarcao <falamarcao@gmail.com>
2024-10-09 19:52:12 -0300

    JS Action Output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -17,6 +17,9 @@ function run() {
 
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
 
+    const websiteURL = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOuput('website-url', websiteURL);
+
  }
 
 run();
